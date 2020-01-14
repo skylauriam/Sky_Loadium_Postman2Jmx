fc8673ddcd7ec359f1e23bac795ae0c0fef1f1a4
LAURIA Matteo <lauriam@lauriam-apple.sky.local>
2020-01-14 17:26:28 +0100

    update sh and jar files

--- fromPostmanToJmxFile.sh
+++ fromPostmanToJmxFile.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-[ ! -d "ita-selfcare-bb-automation_API" ] && git clone https://github.com/sky-uk/ita-selfcare-bb-automation_API.git
-cd ita-selfcare-bb-automation_API
+[ ! -d "ita-api-assistenza-automation_API" ] && git clone https://github.com/sky-uk/ita-api-assistenza-automation_API.git
+cd ita-api-assistenza-automation_API
 git checkout develop
 git pull
 cd ..
-java -jar Postman2Jmx.jar ita-selfcare-bb-automation_API/$1 perfCI_draftVersion.jmx
-java -jar fromPostmanJsonInputDataToSedCmd.jar $3 selfcare ita-selfcare-bb-automation_API/$2
-
-#ita-selfcare-bb-automation_API/$1
+java -jar Postman2Jmx.jar ita-api-assistenza-automation_API/$1 perfCI_draftVersion.jmx
+java -jar fromPostmanJsonInputDataToSedCmd.jar ita-api-assistenza-automation_API/$2
 
